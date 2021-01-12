bb7967ed22872679890278d51c4e704dc8786ae7
AlexanderBowers <7319abowers@gmail.com>
2021-01-12 11:45:44 -0800

    finished lab

--- index.js
+++ index.js
@@ -9,5 +9,25 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+let h1 = document.getElementById("greeting");
+let time = handleClick(e);
+
+function greet(string) {
+  let time = string.substr(0, 2);
+  if (time > 17) {
+    return "Good Evening";
+  }
+  else if (time > 12){
+    return "Good Afternoon";
+  }
+  else {
+    return "Good Morning";
+  }
+}
+
+function displayMessage(string) {
+ h1.innerText = string;
+}
+
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
